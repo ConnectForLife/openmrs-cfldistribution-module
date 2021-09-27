a9f9f2f83a02e24bc2d4bbf0b0bb366d59754fe1
Piotr Wargulak <pwargulak@soldevelo.com>
2021-09-27 15:40:54 +0200

    Published openmrs-cfldistribution-module

---
+++ api/src/main/java/org/openmrs/module/cfldistribution/CfldistributionConstants.java
@@ -0,0 +1,16 @@
+package org.openmrs.module.cfldistribution;
+
+public final class CfldistributionConstants {
+
+    public static final String MODULE_ID = "cfldistribution";
+    public static final String MODULE_API_PACKAGE = "org.openmrs.module.cfldistribution.api";
+
+    /**
+     * The ID of ETL Lite module - an optional CfL distribution module.
+     */
+    public static final String ETL_MODULE_ID = "etllite";
+
+    private CfldistributionConstants() {
+
+    }
+}
